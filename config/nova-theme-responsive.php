9c9bfcf628fe6be61d18c48b250ae26fbb117a29
Ratno Putro Sulistiyono <ratno@knoqdown.com>
2020-04-06 19:09:11 +0700

    config/nova-theme-responsive.php: set default config remove tombol untuk add another atau continue editing, plus set fixed sidebar

--- config/nova-theme-responsive.php
+++ config/nova-theme-responsive.php
@@ -15,18 +15,18 @@ return [
     'resource_tables_sticky_actions_on_mobile' => false,
 
     // If true, hides the "Create & Add Another" button on "Create" forms
-    'hide_create_and_add_another_button' => false,
+    'hide_create_and_add_another_button' => true,
 
     // If true, hides the "Create & Add Another" button on "Create" forms (mobile only)
-    'hide_create_and_add_another_button_on_mobile' => false,
+    'hide_create_and_add_another_button_on_mobile' => true,
     
     // If true, hides the "Update & Continue Editing" button on "Update" forms
-    'hide_update_and_continue_editing_button' => false,
+    'hide_update_and_continue_editing_button' => true,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms (mobile only)
-    'hide_update_and_continue_editing_button_on_mobile' => false,
+    'hide_update_and_continue_editing_button_on_mobile' => true,
 
     // If true, the sidebar will stay fixed on desktop view
-    'fixed_sidebar' => false,
+    'fixed_sidebar' => true,
 
 ];
